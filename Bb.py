94593c7e5714c57a85a82c4cc0e8a9378214108a
ID-Arief <73363090+ID-Arief@users.noreply.github.com>
2020-10-27 18:38:43 +0700

    Update and rename B.py to Bb.py

--- Bb.py
+++ Bb.py
@@ -21,10 +21,10 @@ def main():
 #--------------------------------Banner/LOGO------------------------------------------
 def logo():
   print """%s 
-   ___       _     ___  ____         __%sAuthor by %Arifisal%s
-  / _ | ____(_)__ / _/ /  _/__ ___ _/ /%sGithub %sgithub.com/ID-ARIEF%s   
- / __ |/ __/ / -_) _/ _/ /(_-</ _ `/ /%sTeam %CyberGebang%s 
-/_/ |_/_/ /_/\__/_/  /___/___/\_,_/_/%sTools spam otp dengan 29 spammers"""%(qu,pu,ku,qu,pu,ku,qu,pu,ku,qu,qu) 
+     ___       _     ___  ____         __ %sAuthor by %Arifisal%s
+    / _ | ____(_)__ / _/ /  _/__ ___ _/ / %sGithub %sgithub.com/ID-ARIEF%s   
+   / __ |/ __/ / -_) _/ _/ /(_-</ _ `/ /  %sTeam %CyberGebang%s 
+  /_/ |_/_/ /_/\__/_/  /___/___/\_,_/_/   %sTools spam otp dengan 29 spammers"""%(qu,pu,ku,qu,pu,ku,qu,pu,ku,qu,qu) 
 
 #-------------------------------Input Function------------------------------------------
 def input():
